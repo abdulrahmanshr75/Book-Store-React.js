3b1a2d1906acb6bade95f725c012099cfa27b80a
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-03-23 20:18:27 +0200

    setup linters and readme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
-import Books from "./components/Books";
-import Categories from "./components/Categories";
-import Navbar from "./components/Navbar";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Navbar />
       <Switch>
         <Route exact path="/">
@@ -17,7 +17,7 @@ function App() {
           <Categories />
         </Route>
       </Switch>
-    </React.Fragment>
+    </>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,16 @@
-import React from "react";
+import React from 'react';
 
-const Form = () => {
-  return (
-    <div className="add-book">
-      <h1>Add a new Book</h1>
-      <form className="form">
-        <label className="text-label">Book Title</label>
-        <input className="input" type="text"></input>
-        <label className="text-label">Book Author</label>
-        <input className="input" type="text"></input>
-        <input className="submit-button" type="submit"></input>
-      </form>
-    </div>
-  );
-};
+const Form = () => (
+  <div className="add-book">
+    <h1>Add a new Book</h1>
+    <form className="form">
+      <label className="text-label">Book Title</label>
+      <input className="input" type="text" />
+      <label className="text-label">Book Author</label>
+      <input className="input" type="text" />
+      <input className="submit-button" type="submit" />
+    </form>
+  </div>
+);
 
 export default Form;
